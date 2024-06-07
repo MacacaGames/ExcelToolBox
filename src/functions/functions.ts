a4375f2d3bdea8c843a2668f6101f4e99b2c587f
yurie <yurie@macaca.games>
2024-06-07 16:43:38 +0800

    init

---
+++ src/functions/functions.ts
@@ -0,0 +1,121 @@
+﻿/* global clearInterval, console, CustomFunctions, setInterval */
+
+/**
+ * Adds two numbers.
+ * @customfunction
+ * @param first First number
+ * @param second Second number
+ * @returns The sum of the two numbers.
+ */
+export function add(first: number, second: number): number {
+  return first + second;
+}
+
+// /**
+//  * convert data to Json
+//  * @customfunction
+//  * @param headers Json element name
+//  * @param datas Json element value
+//  * @returns {string} The Json of the data.
+//  */
+// export function tojson(headers: string, datas: string): string {
+//   const objects = getJsonArrayFromData(headers, datas);
+//   const json = JSON.stringify(objects);
+//   if (json.length < 50000) {
+//     return json;
+//   }
+//   return json;
+// }
+
+/**
+ * convert data to Json
+ * @customfunction
+ * @param datas element value
+ * @returns {string} The Json of the data.
+ */
+export function tojson(datas: string): string {
+  return "bla";
+}
+
+async function dataArrayToJson(data: any[][]) {
+  const headers = data[0];
+  const rows = data.slice(1);
+
+  const json = rows.map(row => {
+    const obj = {};
+    headers.forEach((header, index) => {
+      let value = row[index];
+      if(value === "NoData_Int" || value === "NoData_Enum") {
+        value = 0;
+      } else if (value === "NoData_Bool") {
+        value = false;
+      } else if (value === "NoData_Json") {
+        value = {};
+      } else if (value === "NoData_Array") {
+        value = [];
+      } else if (value === "NoData_String") {
+        value = "";
+      }
+      obj[header] = value;
+    });
+    return obj;
+  });
+
+  const jsonString = JSON.stringify(json, null, 2);
+
+  await openJsonDialog(jsonString);
+}
+
+/**
+ * Displays the current time once a second.
+ * @customfunction
+ * @param invocation Custom function handler
+ */
+export function clock(invocation: CustomFunctions.StreamingInvocation<string>): void {
+  const timer = setInterval(() => {
+    const time = currentTime();
+    invocation.setResult(time);
+  }, 1000);
+
+  invocation.onCanceled = () => {
+    clearInterval(timer);
+  };
+}
+
+/**
+ * Returns the current time.
+ * @returns String with the current time formatted for the current locale.
+ */
+export function currentTime(): string {
+  return new Date().toLocaleTimeString();
+}
+
+/**
+ * Increments a value once a second.
+ * @customfunction
+ * @param incrementBy Amount to increment
+ * @param invocation Custom function handler
+ */
+export function increment(incrementBy: number, invocation: CustomFunctions.StreamingInvocation<number>): void {
+  let result = 0;
+  const timer = setInterval(() => {
+    result += incrementBy;
+    invocation.setResult(result);
+  }, 1000);
+
+  invocation.onCanceled = () => {
+    clearInterval(timer);
+  };
+}
+
+/**
+ * Writes a message to console.log().
+ * @customfunction LOG
+ * @param message String to write.
+ * @returns String to write.
+ */
+export function logMessage(message: string): string {
+  console.log(message);
+
+  return message;
+}
---
+++ src/taskpane/taskpane.ts
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+// The initialize function must be run each time a new page is loaded
+Office.onReady(() => {
+  document.getElementById("sideload-msg").style.display = "none";
+  document.getElementById("app-body").style.display = "flex";
+  document.getElementById("selectRangeToJsonButton").onclick = selectRangeToJson;
+  document.getElementById("selectWholeSheetToJsonButton").onclick = selectWholeSheetToJson;
+
+  document.getElementById("insertNoDataIntButton").onclick = insertNoDataInt;
+  document.getElementById("insertNoDataEnumButton").onclick = insertNoDataEnum;
+  document.getElementById("insertNoDataBoolButton").onclick = insertNoDataBool;
+  document.getElementById("insertNoDataArrayButton").onclick = insertNoDataArray;
+  document.getElementById("insertNoDataStringButton").onclick = insertNoDataString;
+});
+
+async function selectRangeToJson() {
+  try {
+    const rangeInput = (document.getElementById("rangeInput") as HTMLInputElement).value.trim();
+
+    if (!rangeInput) {
+      console.error('Please enter a valid range.');
+      return;
+    }
+    
+    await Excel.run(async (context) => {
+      const sheet = context.workbook.worksheets.getActiveWorksheet();
+      const range = sheet.getRange(rangeInput);
+      
+      range.load("values");
+      await context.sync();
+
+      const data = range.values;
+      await dataArrayToJson(data);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function selectWholeSheetToJson() {
+  try {
+    await Excel.run(async (context) => {
+      const sheet = context.workbook.worksheets.getActiveWorksheet();
+      const range = sheet.getUsedRange();
+      
+      range.load("values");
+      await context.sync();
+      
+      const data = range.values;
+      await dataArrayToJson(data);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function dataArrayToJson(data: any[][]) {
+  const headers = data[0];
+  const rows = data.slice(1);
+
+  const json = rows.map(row => {
+    const obj = {};
+    headers.forEach((header, index) => {
+      let value = row[index];
+      if(value === "NoData_Int" || value === "NoData_Enum") {
+        value = 0;
+      } else if (value === "NoData_Bool") {
+        value = false;
+      } else if (value === "NoData_Json") {
+        value = {};
+      } else if (value === "NoData_Array") {
+        value = [];
+      } else if (value === "NoData_String") {
+        value = "";
+      }
+      obj[header] = value;
+    });
+    return obj;
+  });
+
+  const jsonString = JSON.stringify(json, null, 2);
+
+  await openJsonDialog(jsonString);
+}
+
+async function openJsonDialog(jsonString: string) {
+  Office.context.ui.displayDialogAsync(
+      location.origin + '/taskpane/dialog.html?json=' + encodeURIComponent(jsonString),
+      { height: 50, width: 50 },
+      (result) => {
+        if (result.status !== Office.AsyncResultStatus.Succeeded) {
+          console.error('Failed to open dialog:', result.error.message);
+        }
+      }
+  );
+}
+
+async function insertNoDataInt() {
+  await insertData("NoData_Int");
+}
+
+async function insertNoDataEnum() {
+  await insertData("NoData_Enum");
+}
+
+async function insertNoDataBool() {
+  await insertData("NoData_Bool");
+}
+
+async function insertNoDataJson() {
+  await insertData("NoData_Json");
+}
+
+async function insertNoDataArray() {
+  await insertData("NoData_Array");
+}
+
+async function insertNoDataString() {
+  await insertData("NoData_String");
+}
+
+async function insertData(data: any) {
+  try {
+    await Excel.run(async (context) => {
+      const selectedRange = context.workbook.getSelectedRange();
+      selectedRange.values = data;
+      await context.sync();
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
